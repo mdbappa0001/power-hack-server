88a5776e90779f13b0d9f2a84abaa46bd191bab1
mdbappa0001 <abutanif366@gmail.com>
2022-06-30 16:35:40 +0600

    initialization done

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middle wares ------------------- 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jl5j5.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+try{
+    await client.connect();
+    console.log('Database Connected')
+}
+finally{
+
+}
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello From power hack')
