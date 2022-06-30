c0765e8c26a7f94bc9380bf5fa4f26a476134d3f
mdbappa0001 <abutanif366@gmail.com>
2022-07-01 03:01:19 +0600

    deleted api added

--- index.js
+++ index.js
@@ -15,38 +15,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-try{
-    await client.connect();
-    const billCollection = client.db("powerHack").collection("bills");
+async function run() {
+  try {
+      await client.connect();
+      const billCollection = client.db("powerHack").collection("bills");
 
-    app.get('/billing-list', async (req, res) => {
-        const query = {}
-        const page = parseInt(req.query.page)
-        const size = 10;
-        const result = await billCollection.find(query).skip(page*10).limit(size).toArray()
-        res.send({ result })
-    })
+      app.get('/billing-list', async (req, res) => {
+          const query = {}
+          const page = parseInt(req.query.page)
+          const searched = req.query.searched
+          const size = 10;
+          const result = await billCollection.find(query).skip(page * 10).limit(size).toArray()
+          res.send({ result })
+      })
+      app.get('/billingCount', async (req, res) => {
+          const count = await billCollection.estimatedDocumentCount()
+          res.send({ count })
+      })
 
-    app.get('/billingCount', async (req, res) => {
-        const count = await billCollection.estimatedDocumentCount()
-        res.send({ count })
-    })
+      app.post('/add-billing', async (req, res) => {
+          const billing = req.body;
+          const result = await billCollection.insertOne(billing)
+          res.send(result)
+      })
+      app.put('/update-billing/:id', async (req, res) => {
+          const id = req.params.id;
+          const newBill = req.body;
+          const filter = { _id: ObjectId(id) }
+          const options = { upsert: true }
+          const updateDoc = {
+              $set: newBill
+          };
+          const result = await billCollection.updateOne(filter, updateDoc, options)
+          res.send(result)
+      })
 
-    app.delete('/delete-billing/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) }
-        const result = await billCollection.deleteOne(query);
-        res.send(result);
-    })
+      app.delete('/delete-billing/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) }
+          const result = await billCollection.deleteOne(query);
+          res.send(result);
+      })
+  } finally {
 
+  }
 }
-finally{
-
-}
-}
-
 run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
